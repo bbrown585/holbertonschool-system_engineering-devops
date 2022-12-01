5e3e7f80593e88d192954d91407dd620bc6512d4
bbrown585 <brandon_scott585@yahoo.com>
2022-11-30 19:46:33 -0800

    last day of tri

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""A script that fetches the """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+
+    user = requests.get(url + sys.argv[1])
+    reqTodos = requests.get(url + sys.argv[1] + '/todos/')
+
+    jsonemp = user.json()
+    tasks = [key['completed'] for key in reqTodos.json()]
+    print("Employee {} is done with tasks({}/{}):".format(jsonemp.get('name'),
+          tasks.count(True), len(tasks)))
+    for task in reqTodos.json():
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
+
--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status """
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+
+    user = requests.get(url + sys.argv[1])
+    reqTodos = requests.get(url + sys.argv[1] + '/todos/')
+    jsonemp = user.json()
+    filename = str(jsonemp.get('id')) + ".json"
+    tasks = {}
+    tasks[jsonemp.get('id')] = []
+    for todo in reqTodos.json():
+        tasks[jsonemp.get('id')].append({"username": jsonemp.get('username'),
+                                         "task": todo.get('title'),
+                                         "completed": todo.get('completed')})
+
+    with open(filename, 'w') as jsonfile:
+        json.dump(tasks, jsonfile)
